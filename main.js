57392ca3017c5093b0fcb0483ff8864208e1fb73
Robertson Akpan <robertsonakpan@gmail.com>
2022-02-05 23:11:36 +0100

    First commit

---
+++ main.js
@@ -0,0 +1,15 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+
+
+function clickShow(){
+    console.log('This is new show')
+}
+
+clickShow()
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBLFciLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9yZXN0YXVyYW50LWFwcC8uL3NyYy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJcclxuXHJcbmZ1bmN0aW9uIGNsaWNrU2hvdygpe1xyXG4gICAgY29uc29sZS5sb2coJ1RoaXMgaXMgbmV3IHNob3cnKVxyXG59XHJcblxyXG5jbGlja1Nob3coKSJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
