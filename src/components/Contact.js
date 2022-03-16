c2ac74c7bbc2d7902e72ee7058068016d359b26c
Robertson Akpan <robertsonakpan@gmail.com>
2022-03-16 08:42:09 +0100

    Contact section completed

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,22 +1,5 @@
 import './contact.css'
 
-function initMap() {
-    // The location of port Harcourt
-    const portHarcourt = { lat: 4.827567, lng: 6.999109 };
-    // The map, centered at Uluru
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 4,
-      center: portHarcourt,
-    });
-
-    // The marker, positioned at Port Harcourt
-    const marker = new google.maps.Marker({
-        position: portHarcourt,
-        map: map,
-      });
-}
- 
-
 
 const contact = (() => {
 
@@ -26,13 +9,14 @@ const contact = (() => {
         contact.classList.add('contact-background');
         contact.innerHTML = `
         <div class="phone-number">
-        <i class="fa-solid fa-phone-rotary"></i>
+            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="24" height="24" viewBox="0 0 24 24"><path d="M6.62,10.79C8.06,13.62 10.38,15.94 13.21,17.38L15.41,15.18C15.69,14.9 16.08,14.82 16.43,14.93C17.55,15.3 18.75,15.5 20,15.5A1,1 0 0,1 21,16.5V20A1,1 0 0,1 20,21A17,17 0 0,1 3,4A1,1 0 0,1 4,3H7.5A1,1 0 0,1 8.5,4C8.5,5.25 8.7,6.45 9.07,7.57C9.18,7.92 9.1,8.31 8.82,8.59L6.62,10.79Z" /></svg>
             <p>+2348180146992</p>
         </div>
         <div class="address">
+        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="24" height="24" viewBox="0 0 24 24"><path d="M12,11.5A2.5,2.5 0 0,1 9.5,9A2.5,2.5 0 0,1 12,6.5A2.5,2.5 0 0,1 14.5,9A2.5,2.5 0 0,1 12,11.5M12,2A7,7 0 0,0 5,9C5,14.25 12,22 12,22C12,22 19,14.25 19,9A7,7 0 0,0 12,2Z" /></svg>
             <p>509 Gateways Avenue, Port Harcourt, Rivers State, Nigeria</p>
         </div>
-        <div id='map' class="map-display"></div>        
+               
         `;
 
         return contact;
